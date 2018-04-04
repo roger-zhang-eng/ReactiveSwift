a40b59c9e265a243ef82de91bb3d63ba34a5eadf
Roger Zhang <roger.zhang.eng@gmail.com>
2018-04-04 15:07:28 +1000

    Update for Producer and Signal observe values.

--- ReactiveSwift.playground/Pages/Sandbox.xcplaygroundpage/Contents.swift
+++ ReactiveSwift.playground/Pages/Sandbox.xcplaygroundpage/Contents.swift
@@ -17,8 +17,12 @@ import ReactiveSwift
 import Foundation
 import PlaygroundSupport
 
-class ReactiveSolution1 {
+class ReactiveSolutionTest {
     var fsp: SignalProducer<SignalProducer<Int, NoError>, NoError>!
+    
+    init(times:Int) {
+        runTasks(times: times)
+    }
     
     private func superSecretFunc(_ text:String, completion: @escaping (() -> Void)) {
         DispatchQueue.global(qos: .userInitiated).async {
@@ -30,7 +34,7 @@ class ReactiveSolution1 {
         }
     }
     
-    public func runTasks(times:Int) {
+    private func runTasks(times:Int) {
         print("runTasks by \(times) ...")
         var producers = [SignalProducer<Int, NoError>]()
         
@@ -53,24 +57,52 @@ class ReactiveSolution1 {
         
     }
     
-    func observeSignals() {
+    func producerObserveSignals(strategy: FlattenStrategy, completion: (() -> Void)? = nil) {
         // merge will get value once any signal sends, regardless complete.
         // concat will wait the 1st signal value and complete, then accept 2nd signal value, then one by one
         // latest will complete if the last SP completes, only get the last value
-        debugPrint("observeSignals begin to observe by merge...")
-        self.fsp.flatten(.merge)
+        
+        debugPrint("\(#function) begin to observe...")
+        
+        self.fsp.flatten(strategy)
             .on(completed: {
                 print("observeSignals Done!")
+                completion?()
             }, value: { gotValue in
                 print("observeSignals get value: \(gotValue)")
             }).start()
+    }
+    
+    func signalObserveSignals(strategy: FlattenStrategy, completion: (() -> Void)? = nil) {
+        debugPrint("\(#function) begin to observe...")
+        
+        let (isSentValueSignal, isSentValueObserver) = Signal<Int, NoError>.pipe()
+        
+        self.fsp.flatten(strategy).start(isSentValueObserver)
+        
+        isSentValueSignal.observeValues{ (gotValue) in
+            debugPrint("observeSignals get value: \(gotValue)")
+        }
+        
+        isSentValueSignal.observeCompleted {
+            print("observeSignals Done!")
+            completion?()
+        }
     }
 }
 
 //Avoiding playground execution ends earlier than thread processing, need manually run playground and stop it.
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-let rso1 = ReactiveSolution1()
-rso1.runTasks(times: 5)
-rso1.observeSignals()
+let rso1 = ReactiveSolutionTest(times: 5)
+
+/*
+rso1.producerObserveSignals(strategy: .merge) {
+    debugPrint("producerObserveSignals done")
+}
+*/
+
+rso1.signalObserveSignals(strategy: .merge) {
+    debugPrint("signalObserveSignals done")
+}
 
